53e2285af08a503b78c76bfac64ca6d835b66511
Alexandra Staszewski <alexandra_sta@yahoo.de>
2024-10-08 15:10:48 +0200

    Leistungen Seite angelegt

--- page.php
+++ page.php
@@ -13,22 +13,6 @@ get_template_part( 'template-parts/header' );
         
 
     </div>
-
-
-      <!-- Slogan Section -->
-    <section id="slogan-projects">
-        <!-- Hole den Titel aus ACF -->
-        <div class="slogan-container-projects">
-            <div class="schau" id="schau">
-                <?php $slogan1_projects_text = get_field('slogan1_projects_text', get_the_ID()); ?>
-                <h2><?php if( $slogan1_projects_text ): echo esc_html($slogan1_projects_text); endif; ?></h2>
-            </div>
-            <div class="inspiration" id="inspiration">
-                <?php $slogan2_projects_text = get_field('slogan2_projects_text', get_the_ID()); ?>
-                <h2><?php if( $slogan2_projects_text ): echo esc_html($slogan2_projects_text); endif; ?></h2>
-            </div>
-        </div>
-    </section>
     
     
 </main>
